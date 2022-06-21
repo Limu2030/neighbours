8b676e21ab140c02ddb78cae1d4a57d6468d692f
limu2030 <limu2030@gmail.com>
2022-06-22 00:43:40 +0300

    ft: create a url path for add neighbourhood

--- app/urls.py
+++ app/urls.py
@@ -18,5 +18,7 @@ urlpatterns = [
      path('add_post/', views.user_post, name ='add_post'),
 
       path('add_business/<int:neighbourhood_id>', views.create_business, name ='business'),
+
+      path('add_neighbourhood/', views.create_hood, name='neighbourhood'),
   
 ]
