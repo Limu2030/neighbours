135994c5996cd5fa85e4fb56fe2a215858d56a42
limu2030 <limu2030@gmail.com>
2022-06-22 00:51:51 +0300

    ft: create a url path for singlehood

--- app/urls.py
+++ app/urls.py
@@ -9,18 +9,20 @@ urlpatterns = [
 
     path('login/', views.user_login, name='login'),
 
-     path('logout/', views.user_logout, name='logout'),
+    path('logout/', views.user_logout, name='logout'),
     
     path('profile/', views.user_profile, name='profile'),
 
-     path('update_profile/', views.update_profile, name ='update'),
+    path('update_profile/', views.update_profile, name ='update'),
 
-     path('add_post/', views.user_post, name ='add_post'),
+    path('add_post/', views.user_post, name ='add_post'),
 
-      path('add_business/<int:neighbourhood_id>', views.create_business, name ='business'),
+    path('add_business/<int:neighbourhood_id>', views.create_business, name ='business'),
 
-      path('add_neighbourhood/', views.create_hood, name='neighbourhood'),
+    path('add_neighbourhood/', views.create_hood, name='neighbourhood'),
 
-        path('hoods/', views.view_hoods, name ='hoods'),
+    path('hoods/', views.view_hoods, name ='hoods'),
+
+    path('hoods/<int:neighbourhood_id>/', views.singlehood, name='single'),
   
 ]
