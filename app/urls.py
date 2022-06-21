74ccddef9f25e55a9d29cb3c245ed5ffbd58c4e4
limu2030 <limu2030@gmail.com>
2022-06-22 00:33:05 +0300

    ft: create a url path for update profile

--- app/urls.py
+++ app/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
      path('logout/', views.user_logout, name='logout'),
     
     path('profile/', views.user_profile, name='profile'),
+
+     path('update_profile/', views.update_profile, name ='update'),
   
 ]
