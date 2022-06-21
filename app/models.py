1489e17f295f9ae7956a708088bcb01a62d8a16b
limu2030 <limu2030@gmail.com>
2022-06-22 01:13:33 +0300

    ft: create a profile model

--- app/models.py
+++ app/models.py
@@ -28,4 +28,25 @@ class Neighbourhood(models.Model):
         
     def update_neighbourhood(self):
         self.update()
+
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    location = models.CharField(max_length=100)
+    profile_pic=CloudinaryField('profile_pic')
+    username =models.CharField(max_length=100 , null=True)
+    about_me=models.TextField(null=True)
+    # neighbourhood_name=models.CharField(max_length=100)
+    neighbour = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, null=True, blank=True , related_name='jirani')
+    
+    
+    def __str__(self):
+        return self.user.username
+    
+    def save_Profile(self):
+        self.save()
         
+    def delete_Profile(self):
+        self.delete()
+        
+    def update_Profile(self):
+        self.update()        
